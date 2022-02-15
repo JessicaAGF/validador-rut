e723b0ec4c2116089dcbc335adf7f8f0d759fcc7
JessicaAGF <jessica.gajardo@ug.uchile.cl>
2022-02-15 15:23:06 -0300

    k

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var fs         = require('fs');
 var constants  = require('constants');
 var express    = require('express');        // call express
-var https      = require('https');
+var http      = require('http');
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var validations = require('./validationsBeforeStoring');	//actual validation logic
@@ -28,7 +28,7 @@ router.get('/', function(req, res) {
 // register route for DocuWare calls (also accessible at http/https://127.0.0.1:4444/api)
 router.post('/', function(req, res) {
 	var DWInputValues = req.body;
-	
+
 	if (DWInputValues.Values.length > 0) {
 		//call our validations
 		console.log(DWInputValues)
@@ -38,10 +38,10 @@ router.post('/', function(req, res) {
 		})
 		.catch(function (error) {
 			res.json({Status: 'Fail', Reason: error.message });
-		});		
+		});
 	}
 	else{
-		//handle test call from webclient settings	
+		//handle test call from webclient settings
 		res.json({Status: 'OK', Reason: '' });
 	}
 });
@@ -61,12 +61,12 @@ var port = process.env.PORT || 4444;        // set our port
 //app.listen(port);
 
 // HTTPS TLS 1.2 only
-// create cert files openssl req -nodes -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365 -subj "/CN=localhost"  
-https.createServer( {
-		key: fs.readFileSync('key.pem'), 
+// create cert files openssl req -nodes -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365 -subj "/CN=localhost"
+http.createServer( /*{
+		key: fs.readFileSync('key.pem'),
 		cert: fs.readFileSync('cert.pem'),
 		secureOptions: constants.SSL_OP_NO_SSLv2 | constants.SSL_OP_NO_SSLv3 | constants.SSL_OP_NO_TLSv1 | constants.SSL_OP_NO_TLSv1_1
-	}, 
+	},*/
 	app)
 	.listen(port);
 
